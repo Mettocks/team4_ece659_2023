8bd1cb4fc4fd65eaf5634ab58b7862b5fb25ab29
FPGA-Student-Driver <mcohenhoffin@umass.edu>
2023-03-08 15:40:14 -0500

    Adding S-Box Files

---
+++ AES-256_CBC/AES-256_CBC.srcs/sources_1/new/Inverse_S-box.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/08/2023 03:34:45 PM
+// Design Name: 
+// Module Name: Inverse_S-box
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Inverse_S_box(
+    input [127:0] Bytes_In,
+    output [127:0] Bytes_Out
+    );
+endmodule
