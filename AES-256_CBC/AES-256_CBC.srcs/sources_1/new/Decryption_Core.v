5c103914cb3767a92c8ee7a6edcf20b2b7a9d3fb
FPGA-Student-Driver <mcohenhoffin@umass.edu>
2023-03-08 15:29:57 -0500

    Creating Vivado Project

---
+++ AES-256_CBC/AES-256_CBC.srcs/sources_1/new/Decryption_Core.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/08/2023 03:28:34 PM
+// Design Name: 
+// Module Name: Decryption_Core
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Decryption_Core(
+    input CLK
+    );
+endmodule
---
+++ AES-256_CBC/AES-256_CBC.srcs/sources_1/new/Encryption_Core.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/08/2023 03:28:34 PM
+// Design Name: 
+// Module Name: Encryption_Core
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Encryption_Core(
+    input CLK
+    );
+endmodule
