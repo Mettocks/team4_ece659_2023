2ccf629d9d78c963a38d70cbdb819ed0f943de80
FPGA-Student-Driver <mcohenhoffin@umass.edu>
2023-03-28 18:23:01 -0400

    Expansion in Progress Pt 2

--- AES-256_CBC/AES-256_CBC.srcs/sources_1/new/Key_Expansion.v
+++ AES-256_CBC/AES-256_CBC.srcs/sources_1/new/Key_Expansion.v
@@ -23,7 +23,7 @@
 module Key_Expansion(
     input CLK,
     input [255:0] Key,
-    output [13:0][127:0] Expanded_Key //14 Generated 128-bit Keys
+    output [15:0] Expanded_Key [127:0] //14 Generated 128-bit Keys
     );
     
     //Declaring Register Values
@@ -31,12 +31,12 @@ module Key_Expansion(
    // reg Lower_Upper;
    
    // Storing rcon in registers 
-    reg [231:0] rcon  = 112'h0040002000100008000400020001;
-    wire [7:0] rcon_count [6:0];
+    reg [223:0] rcon  = 224'h0040002000100008000400020001;
+    wire [7:0] rcon_cycle [6:0];
     genvar m;  
     
         for (m=0; m<7; m = m + 1) begin
-             assign rcon_cycle[m][7:0] = rcon[m*32 +: 32];
+             assign rcon_cycle[m][31:0] = rcon[m*32 +: 32];
     
         end
     
@@ -44,16 +44,15 @@ module Key_Expansion(
     reg [31:0] temp_schedule; 
     
     reg [5:0] loop_count;
-    reg [3:0] XOR_count;
+    reg [2:0] XOR_count, rcon_count;
+    reg [1:0] Key_Word_Select;
     //reg [7:0][255:0] XYZ;
-    
-  
     
     reg [7:0] current_round_val [31:0]; 
     reg [7:0] previous_round_val [31:0];
     
     reg [1:0] Key_State;
-    localparam Rotate_Left = 2'b00, Sub = 2'b01, XOR_w_rcon = 2'b10;
+    localparam Rotate_Left = 2'b00, Sub = 2'b01, XOR_w_prev = 2'b10, End_Round = 2'b11;
    
    
     always@(posedge CLK, posedge RESET) begin: main_module //Pulling in new Data or asynchronous RESET 
@@ -61,16 +60,18 @@ module Key_Expansion(
     
         if (RESET) begin: pull_data // RESET Circuit when XYZ
     
-        Rotate_Left_Once (Key[255:224], temp);
+        Rotate_Left_Once(Key[255:224], current_round_val[7][31:0]);
         loop_count <= 0;
         Key_State <= Sub;
         XOR_count <= 0;
-        //Expanded_Key[0][31:0] <= Key[31:0];
+        Key_count <= 2;
+        Expanded_Key[0][127:0] <= Key[127:0]; //Double check?
+        Expanded_Key[1][127:0] <= Key[255:128];
         
-            for ( j=0; j<8; j = j+1) begin: pull_key
+            for ( j=0; j<7; j = j+1) begin: pull_key
             
-                current_round_val[j][31:0] <= 32'h00000000;
-                previous_round_val[j][31:0] <= Key[j*32 +: 32];
+                current_round_val[j][23:0] <= 24'h000000;
+               // previous_round_val[j][31:0] <= Key[j*32 +: 32];
             
             end: pull_key
         
@@ -80,76 +81,93 @@ module Key_Expansion(
         else begin: expand_key
     
            if (loop_count < 56) begin: key_loop 
-           //needs work on timing. Is register necessary?
-    
-              if (!(loop_count % 8)) begin: Mod_Eight_Zero
+               
+              if (XOR_count == 0) begin: Mod_Eight_Zero
     
                   case(Key_State)  
                   
                         Rotate_Left: begin
                   
-                            Rotate_Left_Once(previous_round_val[7][31:0], current_round_val[0][31:0]);
+                            Rotate_Left_Once(Expanded_Key[Key_count - 1][127:96], current_round_val[0][31:0]);
                             Key_State <= Sub;
                   
                         end
                   
                         Sub: begin 
                    
-                            S_box(current_round_val, current_round_val);
-                            Key_State <=  XOR_w_rcon;
+                            S_Box_32w(current_round_val[0][31:0], current_round_val[0][31:0]);
+                            Key_State <=  XOR_w_prev;
                       
                         end
     
-                        XOR_w_rcon: begin
+                        XOR_w_prev: begin
                                          
-                      
-                            current_round_val[0][31:0] <= previous_round_val[7][31:0] ^ rcon[rcon_count*32 +: 32];
-                      
-                      
-                      //    for (k=0; k < 8; k = k + 1) begin
-                                
-                                  
-                             
-                               
-//                                if (k == 7) begin
-//                                Key_State <= Rotate_Left;
-//                                loop_count <= loop_count + 1;
-//                                end
-                                             
-                            //end
-                      
-                           
-                        
-                            Key_state <= Rotate_Left;
+                            //  previous_round_val[0][31:0] <= previous_round_val[7][31:0] ^ current_round_val[0][31:0] ^ rcon[rcon_count*32 +: 32];
+                            Expanded_Key[Key_count][31:0] <= Expanded_key[key_count - 2][127:96] ^ current_round_val[0][31:0] ^ rcon[rcon_count*32 +: 32];   //can re-use this register?         
+                            rcon_count <= rcon_count + 1;
+                            XOR_count <= XOR_count + 1;                                                             
+                            //Key_state <= XOR_w_prev; //?
                             
                          end
-                           // Expanded_Key[loop_count][127:0] <= 1;
-    
-                   //end
-                 
+                                  
                  endcase
                  
               end: Mod_Eight_Zero 
+                        
               
+              else if (XOR_count == 4) begin: Mod_Eight_Four
               
-    
-
+                  case(Key_State) 
+                  
+                        Sub: begin 
                    
-                   //end
-                   
-                 
-                   
-                   
+                            S_Box_32w(previous_round_val[4][31:0], current_round_val[4][31:0]);
+                            Key_State <= XOR_w_prev;
+                      
+                        end
+                        
+                        XOR_w_prev: begin
+                                         
+                      
+                         //   previous_round_val[0][31:0] <= previous_round_val[7][31:0] ^ current_round_val[0][31:0] ^ rcon_cycle[rcon_count][31:0];
+                            Expanded_Key[Key_count][31:0] <= Expanded_Key[Key_count - 2][127:96] ^ current_round_val[0][31:0];  
+                            //can re-use the expanded key register?         
+                            XOR_count <= XOR_count + 1;                                                             
+                            
+                            
+                         end
+                        
+                  endcase
                 
-                      //endcase
-                
+              end: Mod_Eight_Four     
+              
+              else begin: XOR_Word
+              
+                   if (XOR_count == 3) begin
+                       Key_count <= Key_count + 1; 
+                  
+                   end
+                  
+                   if (XOR_count == 7) begin
+                   
+                       Key_count <= Key_count + 1; 
+                       XOR_count <= 0;
+                   end   
+                       
+                   else begin
+                    
+                    XOR_count <= XOR_count + 1;  
+                    
+                   end
+                   
+                Expanded_Key[Key_count][31:0] <= Expanded_Key[Key_count - 2][XOR_count*32 +: 32] ^ Expanded_Key[Key_count][(XOR_count - 1)*32 +: 32];    
+               // current_round_val[XOR_count] <= Expanded_Key[Key_count - 1][XOR_count*32 +: 32] ^ current_round_val[XOR_count - 1][31:0];    
+                       
+              end: XOR_Word
                  
-              
-              
-    
-             end: key_loop
+           end: key_loop
             
-             else begin
+           else begin
               
 
               
@@ -157,7 +175,7 @@ module Key_Expansion(
                 RESET <= 1;
                 
               
-             end
+           end
             
         end: expand_key     
              
@@ -169,21 +187,21 @@ endmodule
 
 //blocking or non_blocking?? Is this correct?
 module Rotate_Left_Once(
-    input [7:0][31:0] word_in,
-    output [7:0][31:0] word_out);
+    input [31:0] word_in,
+    output [31:0] word_out);
     
     integer i;     
   
   always @(*) begin  
     
-    for ( i=0; i<8; i = i+1) begin: rotate_left
+    //for ( i=0; i<8; i = i+1) begin: rotate_left
    
-       word_out[i][7:0] <= word_in[i][15:8];
-       word_out[i][15:8] <= word_in[i][23:16];
-       word_out[i][23:16] <= word_in[i][31:24];
-       word_out[i][31:24] <= word_in[i][7:0];
+       word_out[7:0] <= word_in[15:8];
+       word_out[15:8] <= word_in[23:16];
+       word_out[23:16] <= word_in[31:24];
+       word_out[31:24] <= word_in[7:0];
         
-    end: rotate_left
+ //   end: rotate_left
     
   end  
 endmodule
