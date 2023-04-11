627325b88e5cfeee6796afe3b5d6ccc3170240f2
mrcorcoran <mrcorcoran@umass.edu>
2023-04-11 18:20:15 -0400

    Added tineAES keyexapnsion, modified scratch verilog for the new mixcolumns submodules to be used

---
+++ AES-256_CBC/AES-256_CBC.srcs/sources_1/new/tinyAES_keyexpansion.v
@@ -0,0 +1,158 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/10/2023 01:37:57 PM
+// Design Name: 
+// Module Name: tinyAES_keyexpansion
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+/** This wrapper is created to get functional testing going, and is not representitive of the final key expansion **/
+module tinyAES_keyexpansion(
+    input clk,
+    input [255:0] key,
+    output [127:0] Expanded_Key_One, [127:0] Expanded_Key_Two, 
+    output [127:0] Expanded_Key_Three, [127:0] Expanded_Key_Four, 
+    output [127:0] Expanded_Key_Five, [127:0] Expanded_Key_Six,
+    output [127:0] Expanded_Key_Seven, [127:0] Expanded_Key_Eight,
+    output [127:0] Expanded_Key_Nine, [127:0] Expanded_Key_Ten,
+    output [127:0] Expanded_Key_Eleven, [127:0] Expanded_Key_Twelve,
+    output [127:0] Expanded_Key_Thirteen, [127:0] Expanded_Key_Fourteen
+    );
+
+    reg    [255:0] k0, k0a, k1;
+    wire   [255:0] k2, k3, k4, k5, k6, k7, k8,
+                   k9, k10, k11, k12, k13;
+    wire   [127:0] k0b, k1b, k2b, k3b, k4b, k5b, k6b, k7b, k8b,
+                   k9b, k10b, k11b, k12b, k13b;
+
+    wire [127:0] nothing_wire;
+
+    always @ (posedge clk) begin
+        k0 <= key;
+        k0a <= k0;
+        k1 <= k0a;
+    end
+    
+
+    assign k0b = k0a[127:0];
+
+    expand_key_type_A_256
+        a1 (clk, k1, 8'h1, k2, k1b),
+        a3 (clk, k3, 8'h2, k4, k3b),
+        a5 (clk, k5, 8'h4, k6, k5b),
+        a7 (clk, k7, 8'h8, k8, k7b),
+        a9 (clk, k9, 8'h10, k10, k9b),
+        a11 (clk, k11, 8'h20, k12, k11b),
+        a13 (clk, k13, 8'h40,  nothing_wire, k13b);
+
+    expand_key_type_B_256
+        a2 (clk, k2, k3, k2b),
+        a4 (clk, k4, k5, k4b),
+        a6 (clk, k6, k7, k6b),
+        a8 (clk, k8, k9, k8b),
+        a10 (clk, k10, k11, k10b),
+        a12 (clk, k12, k13, k12b);
+
+    assign Expanded_Key_One         = k0b;
+    assign Expanded_Key_Two         = k1b;
+    assign Expanded_Key_Three       = k2b;
+    assign Expanded_Key_Four        = k3b;
+    assign Expanded_Key_Five        = k4b;
+    assign Expanded_Key_Six         = k5b;
+    assign Expanded_Key_Seven       = k6b;
+    assign Expanded_Key_Eight       = k7b;
+    assign Expanded_Key_Nine        = k8b;
+    assign Expanded_Key_Ten         = k9b;
+    assign Expanded_Key_Eleven      = k10b;
+    assign Expanded_Key_Twelve      = k11b;
+    assign Expanded_Key_Thirteen    = k12b;
+    assign Expanded_Key_Fourteen    = k13b;
+
+endmodule
+
+/* expand k0,k1,k2,k3 for every two clock cycles */
+module expand_key_type_A_256 (clk, in, rcon, out_1, out_2);
+    input              clk;
+    input      [255:0] in;
+    input      [7:0]   rcon;
+    output reg [255:0] out_1;
+    output     [127:0] out_2;
+    wire       [31:0]  k0, k1, k2, k3, k4, k5, k6, k7,
+                       v0, v1, v2, v3;
+    reg        [31:0]  k0a, k1a, k2a, k3a, k4a, k5a, k6a, k7a;
+    wire       [31:0]  k0b, k1b, k2b, k3b, k4b, k5b, k6b, k7b, k8a;
+
+    assign {k0, k1, k2, k3, k4, k5, k6, k7} = in;
+    
+    assign v0 = {k0[31:24] ^ rcon, k0[23:0]};
+    assign v1 = v0 ^ k1;
+    assign v2 = v1 ^ k2;
+    assign v3 = v2 ^ k3;
+
+    always @ (posedge clk)
+        {k0a, k1a, k2a, k3a, k4a, k5a, k6a, k7a} <= {v0, v1, v2, v3, k4, k5, k6, k7};
+
+    S_Box_32w
+        S4_0 ({k7[23:0], k7[31:24]}, k8a);
+
+    assign k0b = k0a ^ k8a;
+    assign k1b = k1a ^ k8a;
+    assign k2b = k2a ^ k8a;
+    assign k3b = k3a ^ k8a;
+    assign {k4b, k5b, k6b, k7b} = {k4a, k5a, k6a, k7a};
+
+    always @ (posedge clk)
+        out_1 <= {k0b, k1b, k2b, k3b, k4b, k5b, k6b, k7b};
+
+    assign out_2 = {k0b, k1b, k2b, k3b};
+endmodule
+
+/* expand k4,k5,k6,k7 for every two clock cycles */
+module expand_key_type_B_256 (clk, in, out_1, out_2);
+    input              clk;
+    input      [255:0] in;
+    output reg [255:0] out_1;
+    output     [127:0] out_2;
+    wire       [31:0]  k0, k1, k2, k3, k4, k5, k6, k7,
+                       v5, v6, v7;
+    reg        [31:0]  k0a, k1a, k2a, k3a, k4a, k5a, k6a, k7a;
+    wire       [31:0]  k0b, k1b, k2b, k3b, k4b, k5b, k6b, k7b, k8a;
+
+    assign {k0, k1, k2, k3, k4, k5, k6, k7} = in;
+    
+    assign v5 = k4 ^ k5;
+    assign v6 = v5 ^ k6;
+    assign v7 = v6 ^ k7;
+
+    always @ (posedge clk)
+        {k0a, k1a, k2a, k3a, k4a, k5a, k6a, k7a} <= {k0, k1, k2, k3, k4, v5, v6, v7};
+
+    S_Box_32w
+        S4_0 (k3, k8a);
+
+    assign {k0b, k1b, k2b, k3b} = {k0a, k1a, k2a, k3a};
+    assign k4b = k4a ^ k8a;
+    assign k5b = k5a ^ k8a;
+    assign k6b = k6a ^ k8a;
+    assign k7b = k7a ^ k8a;
+
+    always @ (posedge clk)
+        out_1 <= {k0b, k1b, k2b, k3b, k4b, k5b, k6b, k7b};
+
+    assign out_2 = {k4b, k5b, k6b, k7b};
+endmodule
+
+
--- verilog/scratch_verilog.v
+++ verilog/scratch_verilog.v
@@ -5,11 +5,12 @@ module mix_mat_mul(
     input [31:0] column,
     output [31:0] out
     );
- wire [7:0] i1c1, i1c2, i2c1, i2c2, i3c1, i3c2, i4c1, i4c2;
+
+    wire [7:0] i1c1, i1c2, i2c1, i2c2, i3c1, i3c2, i4c1, i4c2;
     gf_mult 
         c11(column[31:24], 8'h02, i1c1),
         c21(column[23:16], 8'h03, i1c2);
-    assign out[31:24] =i1c1 ^ i1c2 ^ column[15:8] ^ column[7:0] ;
+    assign out[31:24] = i1c1 ^ i1c2 ^ column[15:8] ^ column[7:0] ;
     
  //wire [7:0] i2c1, i2c2;
     gf_mult 
@@ -22,7 +23,7 @@ module mix_mat_mul(
         c13(column[15:8], 8'h02, i3c1),
         c23(column[7:0], 8'h03, i3c2);
 
-    assign out[15:8] = column[31:24] ^ column[23:16]^i3c1^i3c2;
+    assign out[15:8] = column[31:24] ^ column[23:16]^ i3c1 ^ i3c2;
     
 // wire [7:0] i4c1, i4c2;
     gf_mult 
@@ -33,23 +34,20 @@ module mix_mat_mul(
 endmodule
 
 module gf_mult(
-    input [7:0] b,
-    input [7:0] x,
+    input [7:0] b, // the byte from the input array/column
+    input [7:0] c, // constant
     output reg [7:0] out
     );
-reg a1,a2;
+    
     always @(*) begin
-//        out = b * x;
-        if(x[0] == 0) begin
-            out = b*x;
-            out = b ^ out;
-        end
-        if(x[0] == 1) begin
-            a1= b*8'h02;
-            a2 = x^a1;
-            out = b ^ a1 ^a2;
-        end
+        case (c)
+            8'h02: out = double(b); // 2b
+            8'h03: out = double(b) ^ b; // 3b = 2b + b
+        endcase
     end
 
+    function integer double (input [7:0] x);
+        double = (x << 1) ^ (8'h1b & -(x >> 7));
+    endfunction
 
 endmodule
